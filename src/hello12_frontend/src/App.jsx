302cc06f1112d6916663b9815757360911a7568a
sahiti potini <sahitipotini@sahitis-MacBook-Pro.local>
2024-10-20 16:23:34 +0530

    Initial commit.

---
+++ src/hello12_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { hello12_backend } from 'declarations/hello12_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    hello12_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
